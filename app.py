8f83d2540d09752d89c2301d2ffe75c202700d99
KartikGera12 <101552358+KartikGera12@users.noreply.github.com>
2022-04-08 20:46:42 +1000

    Update app.py

--- app.py
+++ app.py
@@ -3,29 +3,33 @@ import os
 from queue import Empty
 from flask import Flask, render_template, request, session
 import json
+
 # Imports
 app = Flask(__name__)
 app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
-
+#Initialising
 @app.route("/", methods=['GET','POST'])
 def init():
     session['temp_db'] = {}
     return "INIT"
 
+#Igesting the data
 @app.route("/ingest", methods=['GET','POST'])
 def ingest():
     data = {**session['temp_db'],**request.get_json()}
     session['temp_db'] = data
     return "Data has been ingested"
 
+#listing all epics and their Ststus
 @app.route("/getbacklog", methods=['GET'])
 def getBacklog():
     data = session['temp_db']
     return data
 
+#Searching the blocked epics from the bug name
 @app.route("/searchbug", methods=['GET','POST'])
 def getBlockedEpicsByBugName():
     data = session['temp_db']
@@ -37,6 +41,7 @@ def getBlockedEpicsByBugName():
 
     return "BLOCKED EPICS ARE: "+ str(blockedEpics).strip('[]')
 
+#listing all Bugs from an Epic
 @app.route("/searchbugbyepic", methods=['GET','POST'])
 def getBugsByEpicName():
     data = session['temp_db']
@@ -54,6 +59,7 @@ def getBugsByEpicName():
 
     return "BLOCKED EPICS ARE: "+ str(listOfBugs).strip('[]')
 
+#adding a new Task or a new Bug in a given Epic
 @app.route("/updateEpic", methods=['GET','POST'])
 def addTaskOrBugInAEpic():
     data = session['temp_db']
@@ -65,6 +71,7 @@ def addTaskOrBugInAEpic():
     session['temp_db'] = data
     return data
 
+#deleteing a Task or a Bug
 @app.route("/deleteTaskOrBug", methods=['GET','POST'])
 def deleteTaskOrBug():
     data = session['temp_db']
@@ -80,6 +87,8 @@ def deleteTaskOrBug():
     session['temp_db'] = data
     return data
 
+
+#Exporting the Backlogs in the Json format
 @app.route("/exportBacklog", methods=['GET','POST'])
 def exportBacklog():
     data = session['temp_db']
